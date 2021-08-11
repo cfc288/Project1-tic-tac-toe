f058f409322edeee4e1759326f5a56cfbc264a5e
carmenfaye28 <carmenfaye28@gmail.com>
2021-08-10 20:31:57 -0500

    buttons started

--- js/app.js
+++ js/app.js
@@ -1,10 +1,116 @@
-// /*<button class="button blue js-blue-button">Blue</button>*/
-// const buttonBlue = document.querySelector(".js-blue-button")
+class Player 
 
-// function changeBackgroundColorToBlue() {
-//   document.body.style.backgroundColor = 'blue'
-// }
 
-// buttonBlue.addEventListener("click", changeBackgroundColorToBlue)
+
+
+
+
+
+
+//----Example Clicks---------------------------
+/*
+<button class="button blue js-blue-button">Blue</button>
+
+ const buttonBlue = document.querySelector(".js-blue-button")
+
+function changeBackgroundColorToBlue() {
+  document.body.style.backgroundColor = 'blue'
+}
+
+buttonBlue.addEventListener("click", changeBackgroundColorToBlue)
+*/
+
+//=============Row1================
+
+const buttonR1C1 = document.querySelector("#row1-col1")
+
+function clickButtonR1C1() {
+    console.log('clickR1C1')
+}
+
+buttonR1C1.addEventListener("click", clickButtonR1C1)
 
 //=============================
+
+const buttonR1C2 = document.querySelector("#row1-col2")
+
+function clickButtonR1C2() {
+    console.log('clickR1C2')
+}
+
+buttonR1C2.addEventListener("click", clickButtonR1C2)
+
+//=============================
+
+const buttonR1C3 = document.querySelector("#row1-col3")
+
+function clickButtonR1C3() {
+    console.log('clickR1C3')
+}
+
+buttonR1C3.addEventListener("click", clickButtonR1C3)
+
+//=============Row2================
+
+const buttonR2C1 = document.querySelector("#row2-col1")
+
+function clickButtonR2C1() {
+    console.log('clickR2C1')
+}
+
+buttonR2C1.addEventListener("click", clickButtonR2C1)
+
+//=============================
+
+const buttonR2C2 = document.querySelector("#row2-col2")
+
+function clickButtonR2C2() {
+    console.log('clickR2C2')
+    document.getElementById("row2-col2").innerHTML = "X";
+}
+
+buttonR2C2.addEventListener("click", clickButtonR2C2)
+
+//=============================
+
+const buttonR2C3 = document.querySelector("#row2-col3")
+
+function clickButtonR2C3() {
+    console.log('clickR2C3')
+}
+
+buttonR2C3.addEventListener("click", clickButtonR2C3)
+
+//=============Row3================
+
+const buttonR3C1 = document.querySelector("#row3-col1")
+
+function clickButtonR3C1() {
+    console.log('clickR3C1')
+}
+
+buttonR3C1.addEventListener("click", clickButtonR3C1)
+
+//=============================
+
+const buttonR3C2 = document.querySelector("#row3-col2")
+
+function clickButtonR3C2() {
+    console.log('clickR3C2')
+}
+
+buttonR3C2.addEventListener("click", clickButtonR3C2)
+
+//=============================
+
+const buttonR3C3 = document.querySelector("#row3-col3")
+
+function clickButtonR3C3() {
+    console.log('clickR3C3')
+}
+
+buttonR3C3.addEventListener("click", clickButtonR3C3)
+//========================================
+
+
+
