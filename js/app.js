3182827514b907078a0a50b984fa7950b4cb9792
carmenfaye28 <carmenfaye28@gmail.com>
2021-08-10 19:48:13 -0500

    css and html started

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// /*<button class="button blue js-blue-button">Blue</button>*/
+// const buttonBlue = document.querySelector(".js-blue-button")
+
+// function changeBackgroundColorToBlue() {
+//   document.body.style.backgroundColor = 'blue'
+// }
+
+// buttonBlue.addEventListener("click", changeBackgroundColorToBlue)
+
+//=============================
