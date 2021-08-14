d67241d10c28e5f597e9d69e42e1d76654e2e56b
carmenfaye28 <carmenfaye28@gmail.com>
2021-08-14 11:02:30 -0500

    refresh button complete

--- js/app.js
+++ js/app.js
@@ -1,41 +1,68 @@
-class Player 
+let player = "null"
 
 
+//========O
 
+const buttonPlayer1 = document.querySelector("#playero")
 
+function playerOne () {
+    player = "O"
+    console.log(player)
 
-
-
-
-//----Example Clicks---------------------------
-/*
-<button class="button blue js-blue-button">Blue</button>
-
- const buttonBlue = document.querySelector(".js-blue-button")
-
-function changeBackgroundColorToBlue() {
-  document.body.style.backgroundColor = 'blue'
 }
 
-buttonBlue.addEventListener("click", changeBackgroundColorToBlue)
-*/
+buttonPlayer1.addEventListener("click", playerOne)
+
+
+//==========X
+
+
+const buttonPlayer2 = document.querySelector("#playerx")
+
+function playerTwo () {
+    player = "X"
+    console.log(player)
+
+}
+
+buttonPlayer2.addEventListener("click", playerTwo)
+
+
 
 //=============Row1================
 
 const buttonR1C1 = document.querySelector("#row1-col1")
 
 function clickButtonR1C1() {
-    console.log('clickR1C1')
-}
+    // console.log('clickR1C1')
+    console.log("#row1-col1")
+    buttonR1C1.style.color = 'black'
 
-buttonR1C1.addEventListener("click", clickButtonR1C1)
+    if (player == "X") {
+    document.getElementById("row1-col1").innerHTML = "X"
+    }
+    else if (player == "O") {
+        document.getElementById("row1-col1").innerHTML = "O"
+    }
+}    
+
+
+buttonR1C1.addEventListener("click",clickButtonR1C1)
 
 //=============================
 
 const buttonR1C2 = document.querySelector("#row1-col2")
 
 function clickButtonR1C2() {
-    console.log('clickR1C2')
+    // console.log('clickR1C2')
+    console.log("#row1-col2")
+    document.querySelector('#row1-col2').style.color = 'black'
+
+    if (player == "X") {
+    document.getElementById("row1-col2").innerHTML = "X" }
+    else if (player == "O") {
+        document.getElementById("row1-col2").innerHTML = "O"
+    }
 }
 
 buttonR1C2.addEventListener("click", clickButtonR1C2)
@@ -45,7 +72,15 @@ buttonR1C2.addEventListener("click", clickButtonR1C2)
 const buttonR1C3 = document.querySelector("#row1-col3")
 
 function clickButtonR1C3() {
-    console.log('clickR1C3')
+    //console.log('clickR1C3')
+    console.log("#row1-col3")
+    document.querySelector('#row1-col3').style.color = 'black'
+    if (player == "X") {
+    document.getElementById("row1-col3").innerHTML = "X"
+    }
+    else if (player == "O") {
+        document.getElementById("row1-col3").innerHTML = "O"
+    }
 }
 
 buttonR1C3.addEventListener("click", clickButtonR1C3)
@@ -55,7 +90,15 @@ buttonR1C3.addEventListener("click", clickButtonR1C3)
 const buttonR2C1 = document.querySelector("#row2-col1")
 
 function clickButtonR2C1() {
-    console.log('clickR2C1')
+    //console.log('clickR2C1')
+    console.log("#row2-col1")
+    document.querySelector('#row2-col1').style.color = 'black'
+    if (player == "X") {
+        document.getElementById("row2-col1").innerHTML = "X"
+        }
+    else if (player == "O") {
+        document.getElementById("row2-col1").innerHTML = "O"
+    }
 }
 
 buttonR2C1.addEventListener("click", clickButtonR2C1)
@@ -65,8 +108,15 @@ buttonR2C1.addEventListener("click", clickButtonR2C1)
 const buttonR2C2 = document.querySelector("#row2-col2")
 
 function clickButtonR2C2() {
-    console.log('clickR2C2')
-    document.getElementById("row2-col2").innerHTML = "X";
+    //console.log('clickR2C2')
+    console.log("#row2-col2")
+    document.querySelector('#row2-col2').style.color = 'black'
+    if (player == "X") {
+        document.getElementById("row2-col2").innerHTML = "X"
+        }
+    else if (player == "O") {
+        document.getElementById("row2-col2").innerHTML = "O"
+    }
 }
 
 buttonR2C2.addEventListener("click", clickButtonR2C2)
@@ -76,7 +126,15 @@ buttonR2C2.addEventListener("click", clickButtonR2C2)
 const buttonR2C3 = document.querySelector("#row2-col3")
 
 function clickButtonR2C3() {
-    console.log('clickR2C3')
+    //console.log('clickR2C3')
+    console.log("#row2-col3")
+    document.querySelector('#row2-col3').style.color = 'black'
+    if (player == "X") {
+        document.getElementById("row2-col3").innerHTML = "X"
+        }
+    else if (player == "O") {
+        document.getElementById("row2-col3").innerHTML = "O"
+    }
 }
 
 buttonR2C3.addEventListener("click", clickButtonR2C3)
@@ -86,7 +144,15 @@ buttonR2C3.addEventListener("click", clickButtonR2C3)
 const buttonR3C1 = document.querySelector("#row3-col1")
 
 function clickButtonR3C1() {
-    console.log('clickR3C1')
+    //console.log('clickR3C1')
+    console.log("#row3-col1")
+    document.querySelector('#row3-col1').style.color = 'black'
+    if (player == "X") {
+        document.getElementById("row3-col1").innerHTML = "X"
+        }
+    else if (player == "O") {
+        document.getElementById("row3-col1").innerHTML = "O"
+    }
 }
 
 buttonR3C1.addEventListener("click", clickButtonR3C1)
@@ -96,7 +162,15 @@ buttonR3C1.addEventListener("click", clickButtonR3C1)
 const buttonR3C2 = document.querySelector("#row3-col2")
 
 function clickButtonR3C2() {
-    console.log('clickR3C2')
+    //console.log('clickR3C2')
+    console.log("#row3-col2")
+    document.querySelector('#row3-col2').style.color = 'black'
+    if (player == "X") {
+        document.getElementById("row3-col2").innerHTML = "X"
+        }
+    else if (player == "O") {
+        document.getElementById("row3-col2").innerHTML = "O"
+    }
 }
 
 buttonR3C2.addEventListener("click", clickButtonR3C2)
@@ -106,7 +180,15 @@ buttonR3C2.addEventListener("click", clickButtonR3C2)
 const buttonR3C3 = document.querySelector("#row3-col3")
 
 function clickButtonR3C3() {
-    console.log('clickR3C3')
+    //console.log('clickR3C3')
+    console.log("#row3-col3")
+    document.querySelector('#row3-col3').style.color = 'black'
+    if (player == "X") {
+        document.getElementById("row3-col3").innerHTML = "X"
+        }
+    else if (player == "O") {
+        document.getElementById("row3-col3").innerHTML = "O"
+    }
 }
 
 buttonR3C3.addEventListener("click", clickButtonR3C3)
@@ -114,3 +196,11 @@ buttonR3C3.addEventListener("click", clickButtonR3C3)
 
 
 
+//======Page Reload=====================
+
+function reload() {
+    location.reload();
+    return false;
+}
+
+//========================================
---
+++ js/psuedocode.js
@@ -0,0 +1,40 @@
+/*win conditions
+
+
+rows: 
+
+row1: 
+id='row1-col1' , id='row1-col2' , id='row1-col3'
+
+row2:
+id='row2-col1' , id='row2-col2' , id='row2-col3'
+
+row3:
+id='row3-col1' , id='row3-col2' , id='row3-col3'
+
+===============
+
+Columns:
+
+column1:
+id='row1-col1' , id='row2-col1' , id='row3-col1'
+
+column2:
+id='row1-col2' , id='row2-col2' , id='row3-col2'
+
+
+column3:
+id='row1-col3' , id='row2-col3' , id='row3-col3'
+
+
+=================
+Diagnols:
+
+diagnol1:
+id='row1-col1' , id='row2-col2' , id='row3-col3'
+
+diagnol3:
+id='row1-col3' , id='row2-col2' , id='row3-col1'
+
+
+*/
